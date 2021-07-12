aafdfe3627bb05de0b3622e232c6a9d2feeea378
ffrozkhan <ffrozkhan@gmail.com>
2021-07-13 01:12:45 +0530

    differences

---
+++ differences.js
@@ -0,0 +1,19 @@
+function getDataVar(){
+    if(true){
+        var name="Subhan";
+    }
+    console.log(name);
+
+}
+getDataVar();
+
+function GetDataConst(){
+    if(true){
+        const pi=3.14;
+        pi=7.45;
+        console.log(pi);
+    }
+}
+GetDataConst();
+
+
