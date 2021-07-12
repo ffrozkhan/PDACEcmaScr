47ccf5d9fd3e7b6580effc11581477f75283e737
ffrozkhan <ffrozkhan@gmail.com>
2021-07-13 01:12:10 +0530

    add.js

---
+++ add.js
@@ -0,0 +1,11 @@
+function addHim(num1, num2){
+var sum= num1 + num2;
+console.log("sum of " + num1, " and " + num2 + " is equals to " +sum);
+// literals
+console.log(`sum of ${num1} and ${num2} is equals to ${sum}`);
+}
+addHim(23, 17);
+addHim(45, 15);
+addHim(75, 25);
+addHim(85, 15);
+
