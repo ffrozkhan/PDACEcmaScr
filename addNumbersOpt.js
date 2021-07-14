7b2d2a58f0a7d9ad594df9c26058765243ee72f8
ffrozkhan <ffrozkhan@gmail.com>
2021-07-14 12:07:53 +0530

    sum numbers using function

---
+++ addNumbersOpt.js
@@ -0,0 +1,12 @@
+function addNumbersOpt(num1, num2){
+    return num1+num2;
+    // const sum= num1 + num2;
+    // return sum;
+}
+console.log(addNumbersOpt(5,8));
+
+console.log( "sum of given numbers are" + addNumbersOpt(56,60));
+
+console.log("sum of 30 and 40 is" + addNumbersOpt(30,40));
+
+console.log(`sum of 40 + 60=${addNumbersOpt(40,60)}`);
